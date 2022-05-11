adbe9e0fecc6ec5029acaf93a84fe8c898ea0c6d
yoonjeong Kim <kim785392@gmail.com>
2022-05-11 17:16:59 +0900

    220511 kyj 근태유형 조회 삽입 연차유형 조회 삽입 근무그룹유형 진행중

--- src/main/java/com/work/workhub/admin/work/model/service/WorkService.java
+++ src/main/java/com/work/workhub/admin/work/model/service/WorkService.java
@@ -5,10 +5,12 @@ import java.util.List;
 import com.work.workhub.admin.work.model.dto.WorkDTO;
 
 public interface WorkService {
-
-//	boolean workWrite(WorkDTO post) throws Exception;
-//
-//	List<WorkDTO> selectAllwork();
+	
+	//근태유형 삽입
+	boolean workWrite(WorkDTO post) throws Exception;
+	
+	//근태유형 조회
+	List<WorkDTO> selectAllwork();
 
 	
 
