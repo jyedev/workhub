f6bab7bfe3fcf665f2cd9aa557f101ff3957849a
tpdud290 <tpdud290@gmail.com>
2022-05-12 17:33:37 +0900

    0512 비밀번호 찾기 기능 MemberController, MemberMapper, MemberServiceImpl 추가(수정중, 주석처리)

--- src/main/java/com/work/workhub/member/member/dao/MemberMapper.java
+++ src/main/java/com/work/workhub/member/member/dao/MemberMapper.java
@@ -1,6 +1,7 @@
 package com.work.workhub.member.member.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.work.workhub.member.member.dto.MemberDTO;
 
@@ -8,5 +9,12 @@ import com.work.workhub.member.member.dto.MemberDTO;
 public interface MemberMapper {
 
 	MemberDTO findMemberById(String username);
+	
+	//=====================해결 중=======================
+	// 비밀번호 변경
+//	@Transactional
+//	public int update_pw(MemberDTO member) throws Exception{
+//		return sqlsession.update("member.update_pw", member);
+//	}
 
 }
