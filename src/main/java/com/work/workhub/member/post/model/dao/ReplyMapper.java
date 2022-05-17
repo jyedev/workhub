2d6c394b12a906d2221725b295d56f431baeb579
imhsol <im.gksthf@gmail.com>
2022-05-17 17:14:01 +0900

    0517 ihs reply

---
+++ src/main/java/com/work/workhub/member/post/model/dao/ReplyMapper.java
@@ -0,0 +1,18 @@
+package com.work.workhub.member.post.model.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.work.workhub.member.post.model.dto.ReplyDTO;
+
+@Mapper
+public interface ReplyMapper {
+
+	int writeReply(ReplyDTO reply);
+
+	int updateReply(ReplyDTO reply);
+
+	List<ReplyDTO> findReplyListByPostNo(Integer postNo);
+
+}
