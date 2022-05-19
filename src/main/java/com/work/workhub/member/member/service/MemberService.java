5c57050aeb21a5950d553707593273f57c0498f2
tpdud290 <tpdud290@gmail.com>
2022-05-19 17:02:59 +0900

    0519 mypage ing commit

--- src/main/java/com/work/workhub/member/member/service/MemberService.java
+++ src/main/java/com/work/workhub/member/member/service/MemberService.java
@@ -13,32 +13,10 @@ public interface MemberService extends UserDetailsService {
 	public void findPw(String memberEmail, String memberId) throws Exception;
 	
 	public int findPwCheck(MemberDTO memberDTO) throws Exception;
+
+	public MemberDTO edit_info(MemberDTO member)throws Exception;
 	
-	// 회원정보 수정
-//	@Override
-//	public MemberDTO update_mypage(MemberDTO member) throws Exception {
-//		manager.update_mypage(member);
-//		return manager.login(member.getId());
-//	}
-	
-	// 비밀번호 변경
-//	@Override
-//	public MemberDTO update_pw(MemberDTO member, String old_pw, HttpServletResponse response) throws Exception {
-//		response.setContentType("text/html;charset=utf-8");
-//		PrintWriter out = response.getWriter();
-//		if(!old_pw.equals(manager.login(member.getId()).getPw())) {
-//			out.println("<script>");
-//			out.println("alert('기존 비밀번호가 다릅니다.');");
-//			out.println("history.go(-1);");
-//			out.println("</script>");
-//			out.close();
-//			return null;
-//		}else {
-//			manager.update_pw(member);
-//			return manager.login(member.getId());
-//		}
-//	}
-	
+	public MemberDTO update_pw(MemberDTO member, String old_pw, HttpServletResponse response) throws Exception;
 }
 
 
--- src/main/java/com/work/workhub/member/member/service/MemberServiceImpl.java
+++ src/main/java/com/work/workhub/member/member/service/MemberServiceImpl.java
@@ -1,7 +1,10 @@
 package com.work.workhub.member.member.service;
 
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
@@ -75,21 +78,38 @@ public class MemberServiceImpl implements MemberService {
 
 	@Override
 	public void findPw(String memberEmail, String memberId) throws Exception {
-		// TODO Auto-generated method stub
+
 		
 	}
-	
-	/*
-	 * @Override public void findPw(String email, String id) throws Exception {
-	 * String memberKey = new TempKey().getKey(6, false); String psw =
-	 * BCrypt.hashpw(memberKey, BCrypt.gensalt()); MemberMapper.findPw(email, id,
-	 * psw); MailUtils sendmail = new MailUtils(mailUtils);
-	 * sendMail.setSubject("[WorkHub 임시 비밀번호 입니다.]"); //메일 제목 sendMail.setText(
-	 * "<h3>임시 비밀번호 발금</h3>" + "<br/>비밀번호 찾기를 통한 임시 비밀번호입니다."+
-	 * "<br/>임시 비밀번호 : <b>"+memberKey+"</b>"+ "<br/>로그인 후 비밀번호를 변경해주세요."+
-	 * "<a href='http://localhost:8001/member/login"+ ">로그인 페이지</a>");
-	 * sendMail.setFrom("[보낼 이메일]", "WorkHub"); sendMail.setTo(memberEmail);
-	 * sendMail.send(); }
-	 */
 
+	@Override
+	public MemberDTO edit_info(MemberDTO member) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MemberDTO update_pw(MemberDTO member, String old_pw, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+	// 회원정보 수정
+	/*
+	 * @Override public MemberDTO edit_info(MemberDTO member) throws Exception {
+	 * manager.update_mypage(member); return manager.login(member.getId()); }
+	 * 
+	 * // 비밀번호 변경
+	 * 
+	 * @Override public MemberDTO update_pw(MemberDTO member, String old_pw,
+	 * HttpServletResponse response) throws Exception {
+	 * response.setContentType("text/html;charset=utf-8"); PrintWriter out =
+	 * response.getWriter();
+	 * if(!old_pw.equals(manager.login(member.getId()).getPw())) {
+	 * out.println("<script>"); out.println("alert('기존 비밀번호가 다릅니다.');");
+	 * out.println("history.go(-1);"); out.println("</script>"); out.close(); return
+	 * null; }else { manager.update_pw(member); return
+	 * manager.login(member.getId()); } }
+	 */
 }
