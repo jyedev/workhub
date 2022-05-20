c589296a1ee0ae05b811a636c217ef8004ba5b34
hayy1 <hyeweon0610@gmail.com>
2022-05-20 17:24:50 +0900

    220520 캘린더 일정 조회 ing commit

---
+++ src/main/java/com/work/workhub/member/calendar/model/dto/CalCategoryDTO.java
@@ -0,0 +1,17 @@
+package com.work.workhub.member.calendar.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CalCategoryDTO {
+	
+	private String calCategoryCode;  //일정카테고리코드 
+	private String calCategoryName;  //일정카테고리이름
+	
+	
+
+}
---
+++ src/main/java/com/work/workhub/member/calendar/model/service/CalendarService.java
@@ -0,0 +1,11 @@
+package com.work.workhub.member.calendar.model.service;
+
+import java.util.List;
+
+import com.work.workhub.member.calendar.model.dto.CalendarDTO;
+
+public interface CalendarService {
+
+	List<CalendarDTO> selectCalendarList();
+
+}
