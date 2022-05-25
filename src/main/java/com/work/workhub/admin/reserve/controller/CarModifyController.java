c90f233996b5aff27390ea3970131b5ddb5b9da9
Areum Jo <chloe1771771@gmail.com>
2022-05-25 09:52:43 +0900

    220525 최종 푸쉬

--- src/main/java/com/work/workhub/admin/reserve/controller/CarModifyController.java
+++ src/main/java/com/work/workhub/admin/reserve/controller/CarModifyController.java
@@ -1,8 +1,6 @@
 package com.work.workhub.admin.reserve.controller;
 import java.util.Locale;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
--- src/main/java/com/work/workhub/admin/reserve/controller/CarSelectController.java
+++ src/main/java/com/work/workhub/admin/reserve/controller/CarSelectController.java
@@ -2,19 +2,14 @@ package com.work.workhub.admin.reserve.controller;
 
 
 import java.util.List;
-import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.work.workhub.admin.reserve.model.dto.CarDTO;
 import com.work.workhub.admin.reserve.model.service.CarService;
@@ -27,7 +22,6 @@ import lombok.extern.slf4j.Slf4j;
 public class CarSelectController {
 	
 	private CarService carService;
-	private MessageSource messageSource;
 	
 	@Autowired
 	public CarSelectController(CarService carService) {
